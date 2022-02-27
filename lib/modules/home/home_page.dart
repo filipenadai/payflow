4f97369751ecbf9cc0c7f20ac8d9aa9c4ab2306a
filipenadai <f.nadai05@gmail.com>
2022-02-27 10:33:16 -0300

    add header and bottom navigator in home page

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,12 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_controller.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final homeController = HomeController();
+  final pages = [
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.blue,
+    )
+  ];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("hO ME")),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(152),
+        child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                  text: "Olá, ",
+                  style: TextStyles.titleRegular,
+                  children: [
+                    TextSpan(
+                      text: "Nadai",
+                      style: TextStyles.titleBoldBackground,
+                    )
+                  ],
+                ),
+                style: TextStyles.titleRegular,
+              ),
+              subtitle: Text(
+                "Mantenha suas contas em dia",
+                style: TextStyles.captionShape,
+              ),
+              trailing: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                  color: AppColors.body,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: pages[homeController.currentPage],
+      bottomNavigationBar: SizedBox(
+        height: 90,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () {
+                homeController.setPage(0);
+                setState(() {});
+              },
+              icon: const Icon(
+                Icons.home,
+                color: AppColors.primary,
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                print("opa");
+              },
+              child: Container(
+                height: 56,
+                width: 56,
+                decoration: BoxDecoration(
+                  color: AppColors.primary,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: const Icon(
+                  Icons.add_box_outlined,
+                  color: AppColors.shape,
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                homeController.setPage(1);
+                setState(() {});
+              },
+              icon: const Icon(
+                Icons.description_outlined,
+                color: AppColors.body,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
